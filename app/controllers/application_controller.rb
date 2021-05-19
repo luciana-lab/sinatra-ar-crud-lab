f3eaa35ecdd8afc8a85cbcdaf541fc572dd3987e
Luciana Sugeta <lulusugeta@gmail.com>
2021-05-19 15:03:05 -0400

    commit tree 282577bc3129c0598b6aabc42fc32ea0ce6d96a8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,54 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    "welcome to CRUD world!"
   end
+
+  #read all instances
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  #create a new instance
+  get '/articles/new' do
+    erb :new
+  end
+
+  #read a specific instance
+  get '/articles/:id' do
+    @article = Article.find_by_id(params[:id])
+    erb :show
+  end
+
+  #update an instance
+  get '/articles/:id/edit' do
+    @article = Article.find_by_id(params[:id])
+    erb :edit
+  end
+
+  #place the code extract from the '/articles/new' form
+  post '/articles' do
+    @article = Article.find_or_create_by(title: params[:title], content: params[:content])
+    redirect to "/articles/#{@article.id}"
+  end
+
+  #place the code extract from the '/article/:id/edit' form
+  patch '/articles/:id' do
+    @article = Article.find_by_id(params[:id])
+    @article.title = params[:title]
+    @article.content = params[:content]
+    @article.save
+
+    redirect to "/articles/#{@article.id}"
+  end
+
+  #delete an object
+  delete '/articles/:id' do
+    @article = Article.find_by_id(params[:id])
+    @article.delete
+
+    redirect to "/articles"
+  end
+
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,20 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+    # attr_accessor :title, :content
+    # @@all = []
+
+    # def initialize(params)
+    #     @title = params[:title]
+    #     @content = params[:content]
+    #     save
+    # end
+
+    # def save
+    #     @@all << self
+    # end
+
+    # def self.all
+    #     @@all
+    # end
 end
